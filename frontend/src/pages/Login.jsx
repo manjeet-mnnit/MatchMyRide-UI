beb8572e408bd27465bf03629c6eb41ba7ac2db8
Manjeet <yadavmanjeet629@gmail.com>
2026-06-11 09:58:00 +0530

    feat: enhance login and registration forms with improved UI and loading states

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useUser } from '../context/useUser.js'
+import { Mail, Lock, Eye, EyeOff } from 'lucide-react'
 import axios from '../api/axiosInstance.js'
 
 export default function Login() {
@@ -9,6 +10,8 @@ export default function Login() {
         password: ''
     })
     const [message, setMessage] = useState('')
+	const [isSubmitting, setIsSubmitting] = useState(false)
+	const [showPassword, setShowPassword] = useState(false)
     const navigate = useNavigate()
     const { isAuthenticated, loading, login } = useUser()
 
@@ -25,6 +28,7 @@ export default function Login() {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setIsSubmitting(true)
         try {
             const res = await axios.post('/users/login', formData)
 
@@ -34,39 +38,57 @@ export default function Login() {
         } catch (err) {
             setMessage(err.response?.data?.message || 'Login failed')
         }
+		finally {
+			setIsSubmitting(false)
+		}
     }
 
     return (
-		<div className="min-h-screen bg-gradient-to-br from-blue-50 to-purple-100 flex items-center justify-center px-4">
-			<div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-8">
-				<h2 className="text-3xl font-bold mb-6 text-center text-blue-700">Login</h2>
+		<div className="min-h-screen bg-gradient-to-br from-blue-50 via-gray-100 to-purple-100 flex items-center justify-center px-4">
+			<div className="w-full max-w-lg bg-white shadow-lg rounded-2xl p-8">
+				<img src="../../public/logo.png" className='mx-auto h-12 w-auto mb-6'></img>
+				<h3 className='text-2xl text-left text-gray mt-8 font-semibold'>Welcome back!</h3>
+				<p className='text-md text-left text-gray-600 mb-8'>Sign in to plan your next ride</p>
 				{message && (
 					<p className="text-center text-red-500 mb-4">{message}</p>
 				)}
-				<form onSubmit={handleSubmit} className="space-y-4">
-					<input
-						name="email"
-						type="email"
-						placeholder="Email"
-						value={formData.email}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-						required
-					/>
-					<input
-						name="password"
-						type="password"
-						placeholder="Password"
-						value={formData.password}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-						required
-					/>
+				<form onSubmit={handleSubmit} className="space-y-6">
+					<div className="relative">
+						<Mail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+						<input
+							name="email"
+							type="email"
+							placeholder="Email"
+							value={formData.email}
+							onChange={handleChange}
+							className="w-full pl-10 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+							required
+						/>
+					</div>
+					<div className="relative">
+						<Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+						<input
+							name="password"
+							type={showPassword ? "text" : "password"}
+							placeholder="Password"
+							value={formData.password}
+							onChange={handleChange}
+							className="w-full pl-10 p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+							required
+						/>
+						<div
+							onClick={() => setShowPassword(!showPassword)}
+							className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5 hover:cursor-pointer hover:text-gray-600"
+						>
+							{showPassword ? <EyeOff /> : <Eye />}
+						</div>
+					</div>
 					<button
+						disabled={isSubmitting}
 						type="submit"
-						className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md hover:cursor-pointer transition"
+						className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md hover:cursor-pointer transition disabled:bg-gray-400 disabled:hover:bg-blue-400"
 					>
-						Login
+						{isSubmitting ? 'Logging in...' : 'Login'}
 					</button>
 				</form>
 				<div className="text-center mt-6">
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from '../api/axiosInstance.js'
 import { useNavigate } from 'react-router-dom'
 import { useUser } from '../context/useUser.js'
+import { User, Users, Mail, Lock, Eye, EyeOff, Mars, Venus, Phone, Camera } from 'lucide-react'
 
 export default function Register(){
     const [formData, setFormData] = useState({
@@ -12,6 +13,8 @@ export default function Register(){
 		contactNumber: '',
 		avatar: null,
 	})
+	const [showPassword, setShowPassword] = useState(false)
+	const [isSubmitting, setIsSubmitting] = useState(false)
     const [message, setMessage] = useState('')
 	const navigate = useNavigate()
 	const { isAuthenticated, loading } = useUser()
@@ -33,6 +36,7 @@ export default function Register(){
 
     const handleSubmit = async (e) => {
 		e.preventDefault()
+		setIsSubmitting(true)
 		const form = new FormData()
 		Object.entries(formData).forEach(([key, val]) => {
 			if (key === 'avatar' && val) form.append('avatar', val)
@@ -44,65 +48,107 @@ export default function Register(){
 			setMessage(res.data.message)
 		} catch (err) {
 			setMessage(err.response?.data?.message || 'Registration failed')
+		} finally {
+			setIsSubmitting(false)
 		}
 	}
 
 	return (
 		<div className="min-h-screen bg-gradient-to-br from-blue-50 via-green-50 to-white flex items-center justify-center px-4 py-8">
 			<div className="w-full max-w-lg bg-white rounded-2xl shadow-xl p-8 sm:p-10">
-				<h2 className="text-3xl sm:text-4xl font-bold text-center text-gray-800 mb-6">
+				<h2 className="text-3xl sm:text-4xl font-semibold text-center text-gray-800 mb-6">
 					Create Your Account
 				</h2>
 				{message && (
 					<p className="mb-4 text-center text-sm text-red-600">{message}</p>
 				)}
-				<form onSubmit={handleSubmit} className="space-y-5">
-					<input
-						name="fullName"
-						type="text"
-						placeholder="Full Name"
-						value={formData.fullName}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-						required
-					/>
-					<input
-						name="email"
-						type="email"
-						placeholder="Email"
-						value={formData.email}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-						required
-					/>
-					<input
-						name="password"
-						type="password"
-						placeholder="Password"
-						value={formData.password}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-						required
-					/>
-					<select
-						name="gender"
-						value={formData.gender}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-xl bg-white hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-400"
-						required
-					>
-						<option value="">Select Gender</option>
-						<option value="Male">Male</option>
-						<option value="Female">Female</option>
-					</select>
-					<input
-						name="contactNumber"
-						type="text"
-						placeholder="Contact Number"
-						value={formData.contactNumber}
-						onChange={handleChange}
-						className="w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-					/>
+				<form disabled={isSubmitting} onSubmit={handleSubmit} className="space-y-5">
+					<div className="relative">
+						<User size={18} className="absolute left-3.5 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" />
+						<input
+							name="fullName"
+							type="text"
+							placeholder="Full Name"
+							value={formData.fullName}
+							onChange={handleChange}
+							className="w-full p-3 pl-11 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+							required
+						/>
+					</div>
+					<div className="relative">
+						<Mail size={18} className="absolute left-3.5 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" />
+						<input
+							name="email"
+							type="email"
+							placeholder="Email"
+							value={formData.email}
+							onChange={handleChange}
+							className="w-full p-3 pl-11 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+							required
+						/>
+					</div>
+					<div className="relative">
+						<Lock size={18} className="absolute left-3.5 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" />
+						<input
+							name="password"
+							type={showPassword ? 'text' : 'password'}
+							placeholder="Password"
+							value={formData.password}
+							onChange={handleChange}
+							className="w-full p-3 pl-11 pr-11 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+							required
+						/>
+						<button
+							type="button"
+							onClick={() => setShowPassword(v => !v)}
+							className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer"
+							tabIndex={-1}
+							aria-label={showPassword ? 'Hide password' : 'Show password'}
+						>
+							{showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+						</button>
+					</div>
+					<div>
+						<p className="text-sm text-gray-500 mb-2">Gender</p>
+						<div className="flex gap-3">
+							<button
+								type="button"
+								onClick={() => setFormData({ ...formData, gender: 'Male' })}
+								className={`flex-1 flex items-center justify-center gap-2 p-3 rounded-xl border cursor-pointer transition ${
+									formData.gender === 'Male'
+										? 'border-blue-500 bg-blue-50 text-blue-600 ring-2 ring-blue-400'
+										: 'border-gray-300 text-gray-500 hover:bg-gray-50'
+								}`}
+							>
+								<User size={18} />
+								<span className="text-sm font-medium">Male</span>
+							</button>
+							<button
+								type="button"
+								onClick={() => setFormData({ ...formData, gender: 'Female' })}
+								className={`flex-1 flex items-center justify-center gap-2 p-3 rounded-xl border cursor-pointer transition ${
+									formData.gender === 'Female'
+										? 'border-blue-500 bg-blue-50 text-blue-600 ring-2 ring-blue-400'
+										: 'border-gray-300 text-gray-500 hover:bg-gray-50'
+								}`}
+							>
+								<Users size={18} />
+								<span className="text-sm font-medium">Female</span>
+							</button>
+						</div>
+						<input type="hidden" name="gender" value={formData.gender} required />
+					</div>
+					<div className="relative">
+						<Phone size={18} className="absolute left-3.5 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" />
+						<input
+							name="contactNumber"
+							type="text"
+							placeholder="Contact Number"
+							value={formData.contactNumber}
+							onChange={handleChange}
+							className="w-full p-3 pl-11 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
+						/>
+					</div>
 					<div className="flex gap-3">
 						<div className="flex-1 border-2 border-none rounded-xl  text-center bg-gray-50 hover:bg-gray-100 transition overflow-hidden">
 							<input
@@ -117,17 +163,19 @@ export default function Register(){
 								<button
 									type="button"
 									onClick={() => document.getElementById('avatar-input').click()}
-									className="w-full bg-blue-500 text-white py-3 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+									className="w-full bg-blue-500 text-white py-3 px-4 rounded-lg hover:cursor-pointer hover:bg-blue-600 transition duration-200 flex items-center justify-center gap-2"
 								>
+									<Camera size={18} />
 									{formData.avatar ? formData.avatar.name : 'Choose Avatar'}
 								</button>
 							</label>
 						</div>
 						<button
 							type="submit"
-							className="flex-1 bg-green-600 text-white py-3 px-4 rounded-lg hover:bg-green-700 transition duration-200 hover:cursor-pointer"
+							disabled={isSubmitting}
+							className="flex-1 bg-green-600 text-white py-3 px-4 rounded-lg hover:bg-green-700 transition duration-200 hover:cursor-pointer disabled:bg-gray-400 disabled:hover:bg-gray-400"
 						>
-							Register
+							{isSubmitting ? 'Registering...' : 'Register'}
 						</button>
 					</div>
 				</form>
